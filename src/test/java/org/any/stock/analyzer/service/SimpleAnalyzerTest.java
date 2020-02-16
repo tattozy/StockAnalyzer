0e5a0b412ccc0bb823c694e20b9603655b27f6b2
Carlos Munoz <Carlos.Munoz@auspost.com.au>
2020-02-16 12:32:32 +1100

    Added gradle config to be able to build/test easily.

---
+++ src/test/java/org/any/stock/analyzer/service/SimpleAnalyzerTest.java
@@ -0,0 +1,14 @@
+package org.any.stock.analyzer.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleAnalyzerTest {
+
+    @Test
+    public void shouldFail() {
+        Assert.fail("Should fail");
+
+    }
+
+}
